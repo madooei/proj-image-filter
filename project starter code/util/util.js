7edf9deeef906a728ebfedcf138d8eff2014b38a
Ali Madooei <alimadooei@gmail.com>
2023-07-11 00:09:23 -0400

    Add `isValidUrl` function

--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -1,6 +1,24 @@
 import fs from "fs";
 import Jimp from "jimp";
+import { URL } from "url";
+import { DEBUG } from "../server.js";
 
+// isValidUrl
+// helper function to check if a url is valid
+// returns true if the input is a valid url and false otherwise
+// INPUTS
+//    inputURL: string - a publicly accessible url
+// RETURNS
+//    boolean indicating fi the inputURL is valid or not
+export function isValidUrl(inputURL) {
+  try {
+    new URL(inputURL);
+    return true;
+  } catch (err) {
+    DEBUG && console.log("🚀 ~ file: util.js:19 ~ isValidUrl ~ err:", err);
+    return false;
+  }
+}
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,7 +27,7 @@ import Jimp from "jimp";
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
- export async function filterImageFromURL(inputURL) {
+export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
       const photo = await Jimp.read(inputURL);
@@ -33,7 +51,7 @@ import Jimp from "jimp";
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
- export async function deleteLocalFiles(files) {
+export async function deleteLocalFiles(files) {
   for (let file of files) {
     fs.unlinkSync(file);
   }
